1cd4498ab8b907a5bfa4b35a2b5377b46c0ea177
pavelstarshinov@gmail.com <pav$sta2>
2023-09-11 23:47:49 +0300

    1st review corrections #2

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 URL_HOST = "https://6cd627ba-248b-4a57-a64e-570ae695e538.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_PATH = "/api/v1/orders/track"
+# поторопился и запушил корректровки сразу в ветку master(название коммита "1st review corrections"),
+# поэтому ввожу этот коммент для новой ветки, как указано в задании
